dc2b546db73df1eb770ff0eee18463b08703ff5e
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-16 23:21:37 +0900

    Exception Handler 추가

---
+++ src/main/java/com/example/board/exception/CustomExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.board.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> customExceptionHandler(CustomException e) {
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(e.getErrorCode().getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.getMessage());
+    }
+}
