6aa30864954ee1985cb31360000de6e392052118
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-17 00:19:59 +0900

    findByEmail 메소드 Optional 변경

--- src/main/java/com/example/board/repository/MemberRepository.java
+++ src/main/java/com/example/board/repository/MemberRepository.java
@@ -4,9 +4,12 @@ import com.example.board.data.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Member findByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 
     boolean existsByNickname(String nickname);
 
