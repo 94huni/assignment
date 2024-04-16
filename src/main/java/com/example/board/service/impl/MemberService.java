a97c5c2cf63c9bce72df437ad7205f8157aa2dbb
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-17 00:18:08 +0900

    getMember 추가 + email 중복확인

--- src/main/java/com/example/board/service/impl/MemberService.java
+++ src/main/java/com/example/board/service/impl/MemberService.java
@@ -3,14 +3,19 @@ package com.example.board.service.impl;
 import com.example.board.data.entity.Member;
 import com.example.board.data.requestDto.MemberSignUp;
 import com.example.board.data.requestDto.MemberUpdate;
+import com.example.board.data.responseDto.MemberResponse;
 
 public interface MemberService {
+
+    MemberResponse getMember(String email);
 
     void signUpMember(MemberSignUp memberSignUp);
 
     boolean validNickname(String nickname);
 
     boolean validPassword(String password, String validPassword);
+
+    boolean validEmail(String email);
 
     void updateMember(MemberUpdate memberUpdate, Member member);
 }
