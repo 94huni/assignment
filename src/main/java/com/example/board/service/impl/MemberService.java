308cee6419b2c36edd34bb1683d25a44ff413813
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-16 23:22:09 +0900

    MemberService 인터페이스

--- src/main/java/com/example/board/service/impl/MemberService.java
+++ src/main/java/com/example/board/service/impl/MemberService.java
@@ -1,4 +1,16 @@
 package com.example.board.service.impl;
 
+import com.example.board.data.entity.Member;
+import com.example.board.data.requestDto.MemberSignUp;
+import com.example.board.data.requestDto.MemberUpdate;
+
 public interface MemberService {
+
+    void signUpMember(MemberSignUp memberSignUp);
+
+    boolean validNickname(String nickname);
+
+    boolean validPassword(String password, String validPassword);
+
+    void updateMember(MemberUpdate memberUpdate, Member member);
 }
