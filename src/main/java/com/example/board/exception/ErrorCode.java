782bcd63959d50664f9bd7cbb94bd8c2436c439c
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-17 00:17:32 +0900

    이메일 중복실패 + 권한 없음 에러메세지 추가

--- src/main/java/com/example/board/exception/ErrorCode.java
+++ src/main/java/com/example/board/exception/ErrorCode.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     DUPLICATED_NICKNAME(HttpStatus.CONFLICT, " nickname is duplicated"),
     DIFFERENT_PASSWORD(HttpStatus.CONFLICT, "password is duplicated"),
-    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email is duplicated");
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email is duplicated"),
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "Email is Not found"),
+    NOT_UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Member is UNAUTHORIZED");
+
 
     private final HttpStatus status;
     private final String message;
