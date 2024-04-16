1f990f254b31dd6796afc39d1eeaa0457f8df002
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-17 00:16:44 +0900

    이메일 형식

--- src/main/java/com/example/board/data/requestDto/SignIn.java
+++ src/main/java/com/example/board/data/requestDto/SignIn.java
@@ -2,8 +2,12 @@ package com.example.board.data.requestDto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class SignIn {
+    @Email(message = "Not in email format")
     private String email;
     private String password;
 }
