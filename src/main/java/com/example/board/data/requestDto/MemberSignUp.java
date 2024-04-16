3b4b731c68e5878543bf7871ecbba5aabe7174a1
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-17 00:16:34 +0900

    Not Null 추가, 전화번호 형식, 이메일형식

--- src/main/java/com/example/board/data/requestDto/MemberSignUp.java
+++ src/main/java/com/example/board/data/requestDto/MemberSignUp.java
@@ -2,19 +2,31 @@ package com.example.board.data.requestDto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 
 @Data
 public class MemberSignUp {
-    
+    @NotNull
     private String userName;
 
+    @NotNull
     private String nickName;
 
+    @NotNull
     private String password;
 
+    @NotNull
     private String validPassword;
 
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$"
+            , message = "Only 10 to 11 digits can be entered")
+    @NotNull
     private String phone;
 
+    @Email(message = "Not in email format")
+    @NotNull
     private String email;
 }
