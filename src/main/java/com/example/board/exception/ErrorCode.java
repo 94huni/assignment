ad5fdf844a07cf7071dbcbc32fb76534a97d51a6
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-18 16:03:58 +0900

    서버에러추가 + 내용변경

--- src/main/java/com/example/board/exception/ErrorCode.java
+++ src/main/java/com/example/board/exception/ErrorCode.java
@@ -13,7 +13,8 @@ public enum ErrorCode {
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "409 Email is duplicated"),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "404 Email is Not found"),
     NOT_FORBIDDEN_MEMBER(HttpStatus.FORBIDDEN, "403 No access rights"),
-    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "401 Please check your password");
+    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "401 Please check your Email or Password"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500 INTERNAL_SERVER_ERROR");
 
 
     private final HttpStatus status;
