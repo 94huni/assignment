c00dffbdfc3299b9e496f8186560cfb6048b15c8
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-18 19:05:02 +0900

    전체글조회 + 제목, 내용 검색어 글조회

--- src/main/java/com/example/board/repository/BoardRepository.java
+++ src/main/java/com/example/board/repository/BoardRepository.java
@@ -1,9 +1,15 @@
 package com.example.board.repository;
 
 import com.example.board.data.entity.Board;
+import com.example.board.data.responseDto.BoardResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+    Page<Board> findAllByBIdDesc(Pageable pageable);
+    Page<Board> findBoardByTitleOrContentContainingOrderByBIdDesc(String keyword, Pageable pageable);
+
 }
