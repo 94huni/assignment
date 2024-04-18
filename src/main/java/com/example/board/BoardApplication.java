90dc10376a2beb5a8a2a2dc8e1110305e9439b58
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-18 16:00:54 +0900

    메소드단위의 보안으로 변경하기위해 추가

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,8 +2,10 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class BoardApplication {
 
     public static void main(String[] args) {
