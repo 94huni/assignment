8ee049b04d1f88c55bb49c6ee24a774e87b6ef1b
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-18 18:58:37 +0900

    게시판 인터페이스

--- src/main/java/com/example/board/service/impl/BoardService.java
+++ src/main/java/com/example/board/service/impl/BoardService.java
@@ -1,7 +1,16 @@
 package com.example.board.service.impl;
 
+import com.example.board.data.entity.Member;
+import com.example.board.data.requestDto.BoardCreate;
+import com.example.board.data.requestDto.BoardUpdate;
 import com.example.board.data.responseDto.BoardResponse;
+import com.example.board.data.responseDto.MemberResponse;
+import org.springframework.data.domain.Page;
 
 public interface BoardService {
     BoardResponse getBoard(int bId);
+    void createBoard(BoardCreate create, Member member);
+    void updateBoard(int bId, BoardUpdate boardUpdate, MemberResponse member);
+    void deleteBoard(int bId, Member member);
+    Page<BoardResponse> boardList(String keyword, int page);
 }
