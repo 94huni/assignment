2b0362cb929df1a3587953f5bf290262e9230e86
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-18 16:04:46 +0900

    토큰 필터 추가

---
+++ src/main/java/com/example/board/config/jwt/JwtTokenFilter.java
@@ -0,0 +1,34 @@
+package com.example.board.config.jwt;
+
+import com.example.board.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtProvider.resolveToken(req);
+        try {
+            if (token != null && jwtProvider.validateToken(token)) {
+                Authentication auth = jwtProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (CustomException e) {
+            SecurityContextHolder.clearContext();
+            res.sendError(e.getErrorCode().getStatus().value(), e.getErrorCode().getMessage());
+            return;
+        }
+        filterChain.doFilter(req, res);
+    }
+}
