0652604105bc3d6fcd5dbfdb9599658e3655ee2b
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-18 19:00:17 +0900

    Page 객체 Response DTO 객체로 바꾸는 기능

--- src/main/java/com/example/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -1,15 +1,29 @@
 package com.example.board.service;
 
 import com.example.board.data.entity.Board;
+import com.example.board.data.entity.Member;
+import com.example.board.data.requestDto.BoardCreate;
+import com.example.board.data.requestDto.BoardUpdate;
 import com.example.board.data.responseDto.BoardResponse;
+import com.example.board.data.responseDto.MemberResponse;
+import com.example.board.exception.CustomException;
+import com.example.board.exception.ErrorCode;
 import com.example.board.repository.BoardRepository;
 import com.example.board.service.impl.BoardService;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service("BoardService")
 @RequiredArgsConstructor
+@Slf4j
 public class BoardServiceImpl implements BoardService {
     private final BoardRepository boardRepository;
 
@@ -22,6 +36,16 @@ public class BoardServiceImpl implements BoardService {
         boardResponse.setUpdateAt(board.getUpdateAt());
 
         return boardResponse;
+    }
+
+    private Page<BoardResponse> toEntity(Page<Board> boards) {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Board.class, BoardResponse.class)
+                .addMappings(mapping -> mapping
+                        .map(src -> src.getMember().getNickname(), BoardResponse::setNickname));
+
+        return boards.map(board -> modelMapper.map(board, BoardResponse.class));
     }
 
     @Override
