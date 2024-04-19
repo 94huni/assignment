d8c1368293ea1bb78840aea44838bb9e508c751c
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-19 15:50:34 +0900

    Validation NotNull 어노테이션추가

--- src/main/java/com/example/board/data/requestDto/SignIn.java
+++ src/main/java/com/example/board/data/requestDto/SignIn.java
@@ -3,11 +3,14 @@ package com.example.board.data.requestDto;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
 public class SignIn {
     @Email(message = "Not in email format")
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 }
