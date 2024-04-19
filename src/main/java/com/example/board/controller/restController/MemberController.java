8c09a0d85056311a6d3c27dc0c76a2d5ce246ab8
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-19 16:18:37 +0900

    Import 정리 + url 정리

--- src/main/java/com/example/board/controller/restController/MemberController.java
+++ src/main/java/com/example/board/controller/restController/MemberController.java
@@ -1,31 +1,23 @@
 package com.example.board.controller.restController;
 
-import com.example.board.data.entity.Member;
 import com.example.board.data.requestDto.MemberSignUp;
 import com.example.board.data.requestDto.MemberUpdate;
 import com.example.board.data.requestDto.SignIn;
-import com.example.board.exception.CustomException;
-import com.example.board.exception.ErrorCode;
-import com.example.board.service.AccountService;
 import com.example.board.service.impl.MemberService;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v2/member")
+@RequestMapping("/api/v1/member")
 @Slf4j
 public class MemberController {
     private final MemberService memberService;
@@ -43,7 +35,7 @@ public class MemberController {
         return ResponseEntity.ok(res);
     }
 
-    @PostMapping("/signUp")
+    @PostMapping("/register")
     public ResponseEntity<String> memberSignUp(@Valid @RequestBody MemberSignUp memberSignUp) {
         memberService.signUpMember(memberSignUp);
 
@@ -52,9 +44,8 @@ public class MemberController {
         return ResponseEntity.ok("Membership registration completed");
     }
 
-    @PutMapping("/update/{email}")
-    public ResponseEntity<String> memberUpdate(@PathVariable String email,
-                                               HttpServletRequest request,
+    @PutMapping("/update")
+    public ResponseEntity<String> memberUpdate(HttpServletRequest request,
                                                @Valid @RequestBody MemberUpdate memberUpdate) {
 
         String token = (String) request.getAttribute("token");
@@ -62,18 +53,13 @@ public class MemberController {
 
         String currentMember = memberService.currentMember(token);
 
-        if (email.equals(currentMember)) {
-            memberService.getMember(email);
-        } else {
-            throw new CustomException(ErrorCode.NOT_FORBIDDEN_MEMBER);
-        }
-
         memberService.updateMember(memberUpdate, token);
         log.info("Current Member email : {}", currentMember);
 
         return ResponseEntity.ok("Successfully changed membership information");
     }
 
+    @Hidden
     @GetMapping("/validNickname")
     public int validNickname(String nickname) {
         if (memberService.validNickname(nickname))
@@ -82,6 +68,7 @@ public class MemberController {
             return 0;
     }
 
+    @Hidden
     @GetMapping("/validEmail")
     public int validEmail(String email) {
         if (memberService.validEmail(email))
@@ -90,6 +77,7 @@ public class MemberController {
             return 0;
     }
 
+    @Hidden
     @GetMapping("/validPassword")
     public int validPassword(String password, String validPassword) {
         if (memberService.validPassword(password, validPassword))
