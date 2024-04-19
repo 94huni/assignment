497f43a6f69a6c828ccd36958cfad89b1b9e3c89
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-19 15:45:55 +0900

    스웨거 권한설정 수정
    AuthenticationProvider Bean 생성

--- src/main/java/com/example/board/config/SecurityConfig.java
+++ src/main/java/com/example/board/config/SecurityConfig.java
@@ -2,54 +2,72 @@ package com.example.board.config;
 
 import com.example.board.config.jwt.JwtFilterConfig;
 import com.example.board.config.jwt.JwtProvider;
+import com.example.board.config.jwt.JwtTokenFilter;
 import com.example.board.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AccountService accountService;
     private final JwtProvider jwtProvider;
+    private final JwtTokenFilter jwtTokenFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.apply(new JwtFilterConfig(jwtProvider));
-//        http.antMatcher("/api/**")
-//                .authorizeRequests()
-//                .antMatchers("/api/**/admin").hasRole("ADMIN")
-//                .antMatchers("/api/**/signUp").permitAll()
-//                .antMatchers("/swagger-ui.html",
-//                        "/swagger-ui/**", "/v2/api-docs",
-//                        "/v3/api-docs", "/swagger-resources/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                /*.authorizeRequests()
-//                .antMatchers("/admin").hasRole("ADMIN")
-//                .and()*/
-//                .formLogin().loginPage("/login").permitAll()
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/login").permitAll();
+        http.authorizeRequests()
+                .antMatchers(/* swagger v2 */
+                        "/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        /* swagger v3 */
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(authenticationProvider());
+
     }
 
-    @Override
+    /*@Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2-docs", "/swagger-ui.html",
-                        "/swagger-ui/**", "/v2/api-docs",
-                        "/v3/api-docs", "/swagger-resources/**",
-                        "/webjars", "/public");
+        web.ignoring().antMatchers(
+                "/v2/api-docs",  "/configuration/ui",
+                "/swagger-resources/**", "/configuration/security",
+                "/swagger-ui.html", "/webjars/**","/swagger/**");
+    }*/
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(accountService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
     @Override
