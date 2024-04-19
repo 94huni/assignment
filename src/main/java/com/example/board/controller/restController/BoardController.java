e6b09bb7386251b9c3e422a3b1b30b329fbc83a5
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-19 15:48:11 +0900

    임포트 정리

--- src/main/java/com/example/board/controller/restController/BoardController.java
+++ src/main/java/com/example/board/controller/restController/BoardController.java
@@ -1,13 +1,19 @@
 package com.example.board.controller.restController;
 
+import com.example.board.data.entity.Member;
+import com.example.board.data.requestDto.BoardCreate;
+import com.example.board.data.requestDto.BoardUpdate;
 import com.example.board.data.responseDto.BoardResponse;
+import com.example.board.data.responseDto.MemberResponse;
 import com.example.board.service.impl.BoardService;
+import com.example.board.service.impl.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
