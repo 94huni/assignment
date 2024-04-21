c430677cd646b50b43b36e9892a18d22b2d1c53c
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-21 16:37:56 +0900

    Principal 객체에서 Member 객체로 받아오는 메소드 추가

--- src/main/java/com/example/board/service/impl/MemberService.java
+++ src/main/java/com/example/board/service/impl/MemberService.java
@@ -6,9 +6,13 @@ import com.example.board.data.requestDto.MemberUpdate;
 import com.example.board.data.requestDto.SignIn;
 import com.example.board.data.responseDto.MemberResponse;
 
+import java.security.Principal;
+
 public interface MemberService {
 
     MemberResponse getMember(String email);
+
+    Member principalMember(Principal principal);
 
     String currentMember(String token);
 
