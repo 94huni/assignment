a9c02739d66cfb66032382a20f88eaf45b4fbd38
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-21 17:58:41 +0900

    Exception 클래스의 핸들러 추가

--- src/main/java/com/example/board/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/board/exception/CustomExceptionHandler.java
@@ -11,18 +11,19 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> customExceptionHandler(CustomException e) {
         return ResponseEntity
-                .ok(new ErrorResponse(e.getErrorCode().getStatus().toString(),
                 .status(e.getErrorCode().getStatus()).body(new ErrorResponse(e.getErrorCode().getStatus().toString(),
                         e.getErrorCode().getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
     public ResponseEntity<ErrorResponse> runtimeExceptionHandler(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(e.getMessage());
                 .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage()));
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage()));
     }
 }
