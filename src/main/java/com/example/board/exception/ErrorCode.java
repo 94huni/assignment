950e2f02f50cbbceba00819f467fa59dc3f2bc31
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-21 16:31:34 +0900

    Exception 반환을 변경하여 메시지 변경

--- src/main/java/com/example/board/exception/ErrorCode.java
+++ src/main/java/com/example/board/exception/ErrorCode.java
@@ -8,15 +8,15 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "409 nickname is duplicated"),
-    DIFFERENT_PASSWORD(HttpStatus.CONFLICT, "409 password is duplicated"),
-    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "409 Email is duplicated"),
-    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "404 Email is Not found"),
-    NOT_FOUND_BOARD(HttpStatus.NOT_FOUND, "404 Board is Not found"),
-    NOT_FORBIDDEN_MEMBER(HttpStatus.FORBIDDEN, "403 No access rights"),
-    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "401 Please check your Email or Password"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500 INTERNAL_SERVER_ERROR"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "400 Bad Request");
+    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "nickname is duplicated"),
+    DIFFERENT_PASSWORD(HttpStatus.CONFLICT, "password is duplicated"),
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "Email is duplicated"),
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "Email is Not found"),
+    NOT_FOUND_BOARD(HttpStatus.NOT_FOUND, "Board is Not found"),
+    NOT_FORBIDDEN_MEMBER(HttpStatus.FORBIDDEN, "No access rights"),
+    UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "Please check your Email or Password"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request");
 
 
     private final HttpStatus status;
