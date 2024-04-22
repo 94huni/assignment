2d1b74adf7d6af4b3b5228d9ade2ef0005149ad8
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-22 16:38:14 +0900

    페이지 불러오기위해 Conroller 교체

--- src/main/java/com/example/board/controller/MainController.java
+++ src/main/java/com/example/board/controller/MainController.java
@@ -1,12 +1,13 @@
 package com.example.board.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/main")
     public String main() {
         return "main";
     }
