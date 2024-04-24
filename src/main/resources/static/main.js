b14f86d60956312a7118874f127bb2ecd9e83ae9
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-24 17:25:45 +0900

    login 버튼 클릭시 login 페이지 불러오기

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,6 +2,28 @@
 $(document).ready(function () {
     let pageNumber = 0;
 
+    $(document).on('click', `#login-request`, function () {
+        $('#continueButton').hide();
+        $(`.card-body`).remove();
+        $(`.pageRequest`).remove();
+        const loginHtml = `
+            
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <button class="btn btn-primary w-100 py-2" id="login-submit">Sign in</button>
+                <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+          
+        `;
+        console.log(loginHtml);
+        $(`.login-page`).append(loginHtml);
+    });
+
     $(document).on('click', '#pageContinueButton', function () {
         $(`.card-body`).remove();
         $(`.pageRequest`).remove();
