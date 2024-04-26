d61a164661899bb447686162cb83176a3a99aff9
훈NOTE\94_hu <94_hun_i@naver.com>
2024-04-26 20:51:02 +0900

    게시판 객체생성 코드

--- src/main/java/com/example/board/service/impl/BoardService.java
+++ src/main/java/com/example/board/service/impl/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.board.service.impl;
 
+import com.example.board.data.entity.Board;
 import com.example.board.data.entity.Member;
 import com.example.board.data.requestDto.BoardCreate;
 import com.example.board.data.requestDto.BoardUpdate;
@@ -8,6 +9,7 @@ import com.example.board.data.responseDto.MemberResponse;
 import org.springframework.data.domain.Page;
 
 public interface BoardService {
+    Board findBoard(int bId);
     BoardResponse getBoard(int bId);
     void createBoard(BoardCreate create, Member member);
     void updateBoard(int bId, BoardUpdate boardUpdate, Member member);
