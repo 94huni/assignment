9fe70b5d2f4a8da93e54a551e2e005df598654ee
훈NOTE\94_hu <94_hun_i@naver.com>
2024-05-05 15:05:47 +0900

    페이지 불러올시 token 저장된 쿠키가있으면

--- src/main/java/com/example/board/controller/MainController.java
+++ src/main/java/com/example/board/controller/MainController.java
@@ -1,14 +1,39 @@
 package com.example.board.controller;
 
+import com.example.board.service.impl.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final MemberService memberService;
+
     @GetMapping("/main")
-    public String main() {
+    public String main(HttpServletRequest request,
+                       Model model) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    // JWT 토큰이 존재하면 로그인된 상태로 간주하고, 사용자 정보를 모델에 추가
+                    String jwtToken = cookie.getValue();
+                    String username = memberService.currentMember(jwtToken);
+
+                    // 사용자 정보를 모델에 추가
+                    model.addAttribute("userEmail", username);
+
+                    break;
+                }
+            }
+        }
         return "main";
     }
 
